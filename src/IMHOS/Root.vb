943a538681afb06a868d8d2330ca59f0bbb09ec5
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-01 04:44:25 -0500

    the ship spins

--- src/IMHOS/Root.vb
+++ src/IMHOS/Root.vb
@@ -8,7 +8,7 @@
     Private textureRegions As TextureRegions
     Private sprites As UI.Sprites
     Private instances As Entities
-    Private shipRotation As IReadValueSource(Of Single)
+    Private shipRotation As IWriteValueSource(Of Single)
     Sub New()
         graphics = New GraphicsDeviceManager(Me)
     End Sub
@@ -38,7 +38,7 @@
             {Constants.Sprites.Hex, (Constants.TextureRegions.Hex, (32.0F, 32.0F), (1.0F, 1.0F), (False, False), 0)},
             {Constants.Sprites.Ship, (Constants.TextureRegions.Ship, (32.0F, 32.0F), (1.0F, 1.0F), (False, False), 0)}
         })
-        shipRotation = New ReadOnlyValueSource(Of Single)(Math.PI * 3.0F / 3.0F)
+        shipRotation = New ReadWriteValueSource(Of Single)(Math.PI * 3.0F / 3.0F)
         instances = New Entities
         instances.Add(New Entity(sprites.Read(Constants.Sprites.Hex), (32.0F, 32.0F), (255, 255, 255, 255), New ReadOnlyValueSource(Of Single)(0.0F)))
         instances.Add(New Entity(sprites.Read(Constants.Sprites.Hex), (32.0F, 96.0F), (255, 255, 255, 255), New ReadOnlyValueSource(Of Single)(0.0F)))
@@ -47,9 +47,11 @@
     End Sub
     Protected Overrides Sub Update(gameTime As GameTime)
         MyBase.Update(gameTime)
+        shipRotation.Write(shipRotation.Read() + 0.01F)
     End Sub
     Protected Overrides Sub Draw(gameTime As GameTime)
         MyBase.Draw(gameTime)
+        GraphicsDevice.Clear(Color.Black)
         spriteBatch.Begin()
         instances.Draw(spriteBatch)
         spriteBatch.End()
