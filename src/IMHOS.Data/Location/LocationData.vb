eb497c5d09f2c490ca51cb5108524f863423fdc8
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-04 12:58:02 -0500

    make locations work with two dictionaries

--- src/IMHOS.Data/Location/LocationData.vb
+++ src/IMHOS.Data/Location/LocationData.vb
@@ -1,24 +1,24 @@
 ﻿Public Class LocationData
     Implements ILocationData
-    Private ReadOnly table As New Dictionary(Of Guid, (String, Guid))
+    Private ReadOnly names As New Dictionary(Of Guid, String)
+    Private ReadOnly vessels As New Dictionary(Of Guid, Guid)
     Public Function Create(name As String, vesselId As Guid) As Guid Implements ILocationData.Create
         Dim id = Guid.NewGuid
-        table(id) = (name, vesselId)
+        names(id) = name
+        vessels(id) = vesselId
         Return id
     End Function
-
     Public Function ReadName(locationId As Guid) As String Implements ILocationData.ReadName
-        Dim record As (String, Guid) = (Nothing, Guid.Empty)
-        If table.TryGetValue(locationId, record) Then
-            Return record.Item1
+        Dim result As String = Nothing
+        If names.TryGetValue(locationId, result) Then
+            Return result
         End If
         Return Nothing
     End Function
-
     Public Function ReadVessel(locationId As Guid) As Guid? Implements ILocationData.ReadVessel
-        Dim record As (String, Guid) = (Nothing, Guid.Empty)
-        If table.TryGetValue(locationId, record) Then
-            Return record.Item2
+        Dim result As Guid = Nothing
+        If vessels.TryGetValue(locationId, result) Then
+            Return result
         End If
         Return Nothing
     End Function
