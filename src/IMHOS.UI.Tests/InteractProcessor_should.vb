c240fcf7188fc7dcb45cb4a5b64304c51bf3f899
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-04 18:02:15 -0500

    interact processor

---
+++ src/IMHOS.UI.Tests/InteractProcessor_should.vb
@@ -0,0 +1,7 @@
+﻿Public Class InteractProcessor_should
+    <Fact>
+    Sub instantiates()
+        Dim subject As IInteractProcessor = New InteractProcessor
+        subject.ShouldNotBeNull
+    End Sub
+End Class
--- src/IMHOS.UI/InteractProcessor.vb
+++ src/IMHOS.UI/InteractProcessor.vb
@@ -1,6 +1,5 @@
 ﻿Public Class InteractProcessor
     Implements IInteractProcessor
     Sub New()
-        Throw New NotImplementedException
     End Sub
 End Class
